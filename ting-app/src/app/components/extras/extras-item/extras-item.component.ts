010dc9612e61c64734f303a826bb64b95197f7da
guilleee <88690005+guilleee@users.noreply.github.com>
2022-10-29 17:24:44 -0300

    mejora items botones

--- ting-app/src/app/components/extras/extras-item/extras-item.component.ts
+++ ting-app/src/app/components/extras/extras-item/extras-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Extra } from 'src/app/interfaces/extras';
 
 @Component({
   selector: 'app-extras-item',
@@ -11,8 +12,11 @@ export class ExtrasItemComponent implements OnInit {
   @Input() name : string = "";
   @Input() id : string = "";
   @Input() img : string = "";
+
   @Output() ExtraEvent = new EventEmitter<any>();
   quantity:number = 0;
+  disablePlus:boolean = false;
+  disableMinus:boolean = true;
 
   constructor() { }
 
@@ -22,14 +26,23 @@ export class ExtrasItemComponent implements OnInit {
   clickPlus(){
     if (this.quantity <= 7){
       this.quantity = this.quantity + 1;
+      this.disableMinus = false;
+      this.disablePlus = false;
       this.ExtraEvent.emit(this.id+this.quantity);
+      if (this.quantity === 8){
+        this.disablePlus = true;
+      }
     }
   }
 
   clickMinus(){
     if (this.quantity >= 1){
       this.quantity = this.quantity - 1;
+      this.disablePlus = false;
       this.ExtraEvent.emit(this.id+this.quantity);
+      if(this.quantity === 0){
+        this.disableMinus = true;
+      }
     }
   }
 }
