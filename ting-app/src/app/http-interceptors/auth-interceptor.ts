7452fd78d990fca0e42ae9060fffd269e6059389
Ignacio Valle <nacho.valle20@gmail.com>
2022-11-15 11:23:11 -0300

    cambios en el interceptor

--- ting-app/src/app/http-interceptors/auth-interceptor.ts
+++ ting-app/src/app/http-interceptors/auth-interceptor.ts
@@ -1,22 +1,35 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+import { catchError, Observable, tap } from "rxjs";
+import { ToastrService } from "ngx-toastr";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
+    constructor(private toastr: ToastrService, private route: Router) { }
     intercept(req: HttpRequest<any>,
-              next: HttpHandler): Observable<HttpEvent<any>> {
+        next: HttpHandler): Observable<HttpEvent<any>> {
 
         const idToken = localStorage.getItem("id_token");
-
+        console.log("Entre al interceptor");
         if (idToken) {
             const cloned = req.clone({
                 headers: req.headers.set("Authorization",
                     "Bearer " + idToken)
             });
 
-            return next.handle(cloned);
+            return next.handle(cloned).pipe(catchError(err => {
+                if (err instanceof HttpErrorResponse) {
+                    if (err.status === 401) {
+                        this.toastr.error("Error 401 - Unauthorized: Sesion expirada");
+                        localStorage.removeItem("id_token");
+                        localStorage.removeItem("expires_at");
+                        this.route.navigate(['/home']);
+                    }
+                }
+                throw err;
+            }));
         }
         else {
             return next.handle(req);
