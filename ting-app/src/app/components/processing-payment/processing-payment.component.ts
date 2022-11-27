719367c93c07cf2b4d068149e37d47b46ee44b5d
Ignacio Valle <nacho.valle20@gmail.com>
2022-11-27 19:34:48 -0300

    borro comentarios y agrego error al procesar pago

--- ting-app/src/app/components/processing-payment/processing-payment.component.ts
+++ ting-app/src/app/components/processing-payment/processing-payment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PurchasesService } from 'src/app/services/purchases.service';
 import { Location } from '@angular/common';
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-processing-payment',
@@ -10,25 +11,30 @@ import { Location } from '@angular/common';
 })
 export class ProcessingPaymentComponent implements OnInit {
 
-  constructor(private router: Router, private purchasesService : PurchasesService, private _location : Location) { }
+  constructor(private router: Router, private purchasesService: PurchasesService, private _location: Location, private toastr: ToastrService) { }
 
   sleep: number = 0;
   approved: boolean = false;
-  
+
   ngOnInit(): void {
     this.purchasesService.payment().subscribe((payment) => {
       this.sleep = payment.time;
       this.approved = payment.approved;
       setTimeout(() => {
-        if(this.approved){
+        if (this.approved) {
           this.router.navigate(['qrcode']);
-        }else{
-          this._location.back();
+        } else {
+          this.toastr.error("Error al procesar el Pago")
+            .onHidden.subscribe(() => {
+              this._location.back();
+            });
+
+
         }
-        
-      }, this.sleep);  
+
+      }, this.sleep);
 
     });
-  } 
+  }
 
 }
